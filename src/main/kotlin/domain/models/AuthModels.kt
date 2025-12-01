27bab54ddf6a9cce0ea6fb2ddd9bee1e5c40ba17
Jose Calvo <jacalvoaguilar@gmail.com>
2025-11-30 20:02:27 -0600

    Implementación del modelo de Auth

--- src/main/kotlin/domain/models/AuthModels.kt
+++ src/main/kotlin/domain/models/AuthModels.kt
@@ -1,4 +1,19 @@
 package com.sylvara.domain.models
 
-class AuthModels {
-}
+import io.ktor.http.ContentType
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class LoginRequest(
+    val email: String,
+    val password: String
+)
+
+
+@Serializable
+data class LoginResponse(
+    val token: String,
+    val userId: Int,
+    val email: String,
+    val messages: String = "Login exitoso"
+)
